e8f46053bd0786e7163831e08e87fbcc7f2cd981
peterwaf <peterwafulah@gmail.com>
2024-03-08 08:23:27 +0300

    add scripts

---
+++ Js/script.js
@@ -0,0 +1,145 @@
+let product1 = {
+    name: "Lip Balm Costa",
+    price: 178,
+    details: "Lorem IpsumLorem, ipsum dolor sit amet consectetur adipisicing elit. Distinctio placeat, error quibusdam iure quos esse optio laborum voluptate, officiis explicabo ea nemo inventore laboriosam? Blanditiis quis obcaecati hic similique consequatur!",
+    image: "images/best_sellers/cover-1-271x281.jpg",
+    tag1: "Accessories",
+    tag2: "Lips",
+    tag3: "Make Up"
+}
+
+let product2 = {
+    name: "Better Roses",
+    price: 145,
+    details: "Lorem IpsumLorem, ipsum dolor sit amet consectetur adipisicing elit. Distinctio placeat, error quibusdam iure quos esse optio laborum voluptate, officiis explicabo ea nemo inventore laboriosam? Blanditiis quis obcaecati hic similique consequatur!",
+    image: "/images/best_sellers/Cover-15-271x281.jpg",
+    tag1: "Accessories",
+    tag2: "Lips",
+    tag3: "Others"
+}
+
+let product3 = {
+    name: "Magical Roses",
+    price: 354,
+    details: "Lorem IpsumLorem, ipsum dolor sit amet consectetur adipisicing elit. Distinctio placeat, error quibusdam iure quos esse optio laborum voluptate, officiis explicabo ea nemo inventore laboriosam? Blanditiis quis obcaecati hic similique consequatur!",
+    image: "images/best_sellers/Cover-16-271x281.jpg",
+    tag1: "Accessories",
+    tag2: "Lips",
+    tag3: "Others"
+}
+
+let product4 = {
+    name: "Mexican Texture",
+    price: 205,
+    details: "Lorem IpsumLorem, ipsum dolor sit amet consectetur adipisicing elit. Distinctio placeat, error quibusdam iure quos esse optio laborum voluptate, officiis explicabo ea nemo inventore laboriosam? Blanditiis quis obcaecati hic similique consequatur!",
+    image: "images/best_sellers/Cover-17-271x281.jpg",
+    tag1: "Accessories",
+    tag2: "Lips",
+    tag3: "Others"
+}
+
+let product5 = {
+    name: "South Africa Texture",
+    price: 478,
+    details: "Lorem IpsumLorem, ipsum dolor sit amet consectetur adipisicing elit. Distinctio placeat, error quibusdam iure quos esse optio laborum voluptate, officiis explicabo ea nemo inventore laboriosam? Blanditiis quis obcaecati hic similique consequatur!",
+    image: "images/best_sellers/Cover-19-271x281.jpg",
+    tag1: "Accessories",
+    tag2: "Lips",
+    tag3: "Others"
+}
+
+
+let product6 = {
+    name: "Hills Ingredient",
+    price: 506,
+    details: "Lorem IpsumLorem, ipsum dolor sit amet consectetur adipisicing elit. Distinctio placeat, error quibusdam iure quos esse optio laborum voluptate, officiis explicabo ea nemo inventore laboriosam? Blanditiis quis obcaecati hic similique consequatur!",
+    image: "images/best_sellers/Cover-22-271x281.jpg",
+    tag1: "Accessories",
+    tag2: "Lips",
+    tag3: "Ingredient"
+}
+
+
+let product7 = {
+    name: "Organic Ingredient",
+    price: 587,
+    details: "Lorem IpsumLorem, ipsum dolor sit amet consectetur adipisicing elit. Distinctio placeat, error quibusdam iure quos esse optio laborum voluptate, officiis explicabo ea nemo inventore laboriosam? Blanditiis quis obcaecati hic similique consequatur!",
+    image: "images/best_sellers/Cover-18-271x281.jpg",
+    tag1: "Accessories",
+    tag2: "Lips",
+    tag3: "Organic"
+}
+
+
+let product8 = {
+    name: "Amazon Ingredient",
+    price: 900,
+    details: "Lorem IpsumLorem, ipsum dolor sit amet consectetur adipisicing elit. Distinctio placeat, error quibusdam iure quos esse optio laborum voluptate, officiis explicabo ea nemo inventore laboriosam? Blanditiis quis obcaecati hic similique consequatur!",
+    image: "images/best_sellers/cover-4-271x281.jpg",
+    tag1: "Accessories",
+    tag2: "Lips",
+    tag3: "Organic"
+}
+
+
+document.getElementById("name1").innerText = product1.name;
+document.getElementById("price1").innerText = "KES " + product1.price;
+document.getElementById("image1").src = product1.image;
+document.getElementById("tag1").innerText = product1.tag1;
+document.getElementById("tag2").innerText = product1.tag2;
+document.getElementById("tag3").innerText = product1.tag3;
+
+document.getElementById("name2").innerText = product2.name;
+document.getElementById("price2").innerText = "KES " + product2.price;
+document.getElementById("image2").src = product2.image;
+document.getElementById("tag4").innerText = product2.tag1;
+document.getElementById("tag5").innerText = product2.tag2;
+document.getElementById("tag6").innerText = product2.tag3;
+
+document.getElementById("name3").innerText = product3.name;
+document.getElementById("price3").innerText = "KES " + product3.price;
+document.getElementById("image3").src = product3.image;
+document.getElementById("tag7").innerText = product3.tag1;
+document.getElementById("tag8").innerText = product3.tag2;
+document.getElementById("tag9").innerText = product3.tag3;
+
+document.getElementById("name4").innerText = product4.name;
+document.getElementById("price4").innerText = "KES " + product4.price;
+document.getElementById("image4").src = product4.image;
+document.getElementById("tag10").innerText = product4.tag1;
+document.getElementById("tag11").innerText = product4.tag2;
+document.getElementById("tag12").innerText = product4.tag3;
+
+document.getElementById("name5").innerText = product5.name;
+document.getElementById("price5").innerText = "KES " + product5.price;
+document.getElementById("image5").src = product5.image;
+document.getElementById("tag13").innerText = product5.tag1;
+document.getElementById("tag14").innerText = product5.tag2;
+document.getElementById("tag15").innerText = product5.tag3;
+
+
+document.getElementById("name6").innerText = product6.name;
+document.getElementById("price6").innerText = "KES " + product6.price;
+document.getElementById("image6").src = product6.image;
+document.getElementById("tag16").innerText = product6.tag1;
+document.getElementById("tag17").innerText = product6.tag2;
+document.getElementById("tag18").innerText = product6.tag3;
+
+
+document.getElementById("name7").innerText = product7.name;
+document.getElementById("price7").innerText = "KES " + product7.price;
+document.getElementById("image7").src = product7.image;
+document.getElementById("tag19").innerText = product7.tag1;
+document.getElementById("tag20").innerText = product7.tag2;
+document.getElementById("tag21").innerText = product7.tag3;
+
+document.getElementById("name8").innerText = product8.name;
+document.getElementById("price8").innerText = "KES " + product8.price;
+document.getElementById("image8").src = product8.image;
+document.getElementById("tag22").innerText = product8.tag1;
+document.getElementById("tag23").innerText = product8.tag2;
+document.getElementById("tag24").innerText = product8.tag3;
+
+
+
+
